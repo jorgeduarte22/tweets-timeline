0e0c1b1434db6a11aff8000bb6b85c8054b96c96
Jorge Duarte <jorgeduarte22@gmail.com>
2025-04-17 00:26:28 +0200

    Add sentry publishing

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://57d9d4bac8c474218805411af088b19d@o4508875005296640.ingest.de.sentry.io/4509165295632464"
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
